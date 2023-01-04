703fcf05e4fb0c86cf448422fd052b11e2918ff7
Yichun Zhao <yichun.zhao@autoremind.com>
2023-01-04 04:29:23 +0100

    rm lines

--- src/main/java/com.ynz.sandbox.hidestaticclass/HideStaticClass.java
+++ src/main/java/com.ynz.sandbox.hidestaticclass/HideStaticClass.java
@@ -3,10 +3,6 @@ package com.ynz.sandbox.hidestaticclass;
 public class HideStaticClass {
 
     public static void main(String[] args) {
-        ClientModule clientModule = new MyClientModule();
-        ClientModule standardClientModule = new StandardClientModule();
-        ClientModule myClientModule = new MyClientModule();
-
 
         new ClientModule.Offset();
         new StandardClientModule.Offset();
